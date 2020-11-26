cea8396c96cde0f3bc1d0ebb0af9ed14b43eb7d9
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-26 12:30:10 +0000

    Test to return hello ~wrld~ passes basic test

---
+++ src/Spellchecker.js
@@ -0,0 +1,12 @@
+class Spellchecker {
+
+  checkString(string) {
+    var stringsplit = string.split(" ")
+    // return stringsplit[0].split("")
+
+    return stringsplit[0]+" ~"+stringsplit[1]+"~"
+  }
+
+
+
+}
