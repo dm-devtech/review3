9c658a7b93222df5a28249a295a4b46f3180495d
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-01-15 16:17:41 +0000

    Reformatted tests

--- spec/TestFile.js
+++ spec/TestFile.js
@@ -1,8 +1,8 @@
 describe("Spellchecker", function() {
 
-it('When passed a string with an error it highlights incorrect word', function() {
-  newSpellChecker = new Spellchecker
-  expect(newSpellChecker.checkString("hello wrld")).toEqual("hello ~wrld~")
-})
+  it('When passed a string with an error it highlights incorrect word', function() {
+    newSpellChecker = new Spellchecker
+    expect(newSpellChecker.checkString("hello wrld")).toEqual("hello ~wrld~")
+  })
 
-  });
+});
