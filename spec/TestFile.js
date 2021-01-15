cafc16914acb5a603a6101ebf2160444437a587e
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-01-15 16:22:05 +0000

    added second test to check longer string.  Tests pass

--- spec/TestFile.js
+++ spec/TestFile.js
@@ -5,4 +5,9 @@ describe("Spellchecker", function() {
     expect(newSpellChecker.checkString("hello wrld")).toEqual("hello ~wrld~")
   })
 
+  it('When passed a string with an error it highlights incorrect word', function() {
+    newSpellChecker = new Spellchecker
+    expect(newSpellChecker.checkString("hello wrld hello")).toEqual("hello ~wrld~ hello")
+  })
+
 });
